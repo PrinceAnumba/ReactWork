70f5d755ee3be351b6dce6347443b179909fadb7
Prince Anumba <45015733+PrinceAnumba@users.noreply.github.com>
2022-07-07 13:25:26 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="header" contentEditable="false" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
