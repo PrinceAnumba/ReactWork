28f179873975c516a5d7116c67a8f89d2fa0016c
Prince Anumba <45015733+PrinceAnumba@users.noreply.github.com>
2022-07-08 09:06:49 +0000

    lorem picsum images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+const image = "https://picsum.photos/seed/picsum/200/300";
 ReactDOM.render(
   <div>
     <h1 className="header" contentEditable="false" spellCheck="false">
       My Favourite Foods
     </h1>
     <div>
-      <img />
+      <img src={image + "?grayscale"} alt="" />
+      <img
+        src="https://www.tastingtable.com/img/gallery/mexican-kale-salad-recipe/intro-1647872367.webp"
+        alt=""
+      />
+      <img
+        src="https://www.tastingtable.com/img/gallery/mexican-kale-salad-recipe/gather-the-ingredients-for-mexican-kale-salad-1647872367.webp"
+        alt=""
+      />
+      <img
+        src="https://www.tastingtable.com/img/gallery/mexican-kale-salad-recipe/assemble-the-salad-and-make-the-dressing-1647872367.webp"
+        alt=""
+      />
     </div>
   </div>,
   document.getElementById("root")
